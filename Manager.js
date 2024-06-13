263e5127123fa9b883357f4e2954e2e99a007dce
kjykjypopo <kjy011717@gmail.com>
2024-06-14 07:46:57 +0900

    Delete Manager.js

--- Manager.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loadDataBtn').addEventListener('click', function() {
-    fetch('/api/data') // 서버의 특정 엔드포인트로 요청을 보냄
-        .then(response => response.json())
-        .then(data => {
-            const dataContainer = document.getElementById('dataContainer');
-            dataContainer.innerHTML = `<p>Data: ${data.message}</p>`;
-        })
-        .catch(error => console.error('Error:', error));
-});
